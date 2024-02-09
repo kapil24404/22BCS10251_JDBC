3dac5bfbfb4bb1eb04bdbb93a6ad978480a77893
Kapil Garg <gargkapilgarg1@gmail.com>
2024-02-09 11:03:23 +0530

    final commit

---
+++ src/test/java/com/BCS10251_JDBC/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BCS10251_JDBC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
